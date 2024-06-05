33b6bc1fd5d9f0a855ce32748c5f3859cebb0994
mkpdm <david@mcphee.dev>
2024-06-05 14:37:01 +0100

    docs: design fundamentals & references

--- api/src/app/__main__.py
+++ api/src/app/__main__.py
@@ -4,6 +4,15 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
 app = FastAPI()
+"""Design Fundamentals:
+
+    - use hyphens for uri's (/users/items-in-the-box) to allow for crawlers/indexing.
+    - use snake_case for keys in models. (increase readability)
+    - keep routes in the same routers (don't split /users/ across multiple routers/modules)
+    - keep TTR to < 5s. Use callbacks or websockets for long lived connections.
+    - https://learn.microsoft.com/en-us/azure/architecture/best-practices/api-design
+    - https://www.ecma-international.org/wp-content/uploads/ECMA-404_2nd_edition_december_2017.pdf#page=11
+"""
 
 
 @app.get("/favicon.ico", include_in_schema=False)
