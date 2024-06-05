c95749bf9fc8768456a8275a5b7ad310c7b5766b
mkpdm <david@mcphee.dev>
2024-06-05 14:13:15 +0100

    feat: api boilerplate

---
+++ api/src/app/__main__.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    icon = Path(__file__).parent.absolute() / "public/icons/favicon-32x32.png"
+    return FileResponse(icon)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
